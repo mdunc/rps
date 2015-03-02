27eeca86b58afa508db8a7be465e08690065737b
Mark Duncan <mark.duncan85@gmail.com>
2015-03-02 21:20:10 +0900

    add computer choice and check

--- rps.rb
+++ rps.rb
@@ -1,4 +1,5 @@
 require 'sinatra'
+require 'json'
 
 get '/hi' do
   "Hello I'm Chiaki!"
@@ -6,4 +7,12 @@ end
 
 get '/bye' do
   "See you later!"
+end
+
+get '/computer_choice' do
+  ["rock", "paper", "scissors"].sample.to_json
+end
+
+get '/check/:user_choice/:computer_choice' do
+  "User picked #{params[:user_choice]} and computer picked #{params[:computer_choice]}"
 end
